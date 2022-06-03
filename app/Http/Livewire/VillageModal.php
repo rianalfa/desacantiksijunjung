29b847d4644ba0ed9a84fa425714ecda459c3172
iclc8212@iclasseducation.id <221810560@stis.ac.id>
2022-06-03 11:58:49 +0700

    village modal fixed

--- app/Http/Livewire/VillageModal.php
+++ app/Http/Livewire/VillageModal.php
@@ -13,13 +13,15 @@ class VillageModal extends ModalComponent
     public $village;
     public $districts;
 
-    protected $rules = [
-        'village.id_desa' => 'required|numeric|digits:10|unique:villages,id_desa',
-        'village.name' => 'required|max:50|unique:villages,name',
-        'village.code' => 'required|integer',
-        'village.district_id' => 'required|exists:districts,id',
-        'village.description' => 'nullable',
-    ];
+    public function rules() {
+        return [
+            'village.id_desa' => 'required|unique:villages,id_desa,'.$this->village->id,
+            'village.name' => 'required|max:50|unique:villages,name,'.$this->village->id,
+            'village.code' => 'required|integer',
+            'village.district_id' => 'required|exists:districts,id',
+            'village.description' => 'nullable',
+        ];
+    }
 
     public function mount($id) {
         $this->village = Village::whereId($id)->first() ?? new Village();
